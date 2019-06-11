04a15df37e9b661adc9f3b71510a03fdbc853eca
Leonardo Barbetta <leo_barbetta@outlook.com>
2019-06-11 12:25:25 -0300

    Mudancas

--- Produto.cs
+++ Produto.cs
@@ -1,6 +1,7 @@
 public class Produto{
 
-    public int id {get; set;}
-    public string nome {get; set;}
+    public int Id {get; set;}
+    public string Nome {get; set;}
+    public int Preco {get; set;}
     public Categoria Categoria {get; set;}
 }
